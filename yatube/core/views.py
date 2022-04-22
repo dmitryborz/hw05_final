57dfbfbaf2cfe82a1678ca42d9937bd0af81d363
Дмитрий Борзых <tt7rocky@gmail.com>
2022-04-23 02:17:36 +0300

    1.6.4

--- yatube/core/views.py
+++ yatube/core/views.py
@@ -11,3 +11,7 @@ def csrf_failure(request, reason=''):
 
 def permission_denied(request, exception):
     return render(request, 'core/403.html', status=403)
+
+
+def server_error(request):
+    return render(request, 'core/500.html', status=500)
